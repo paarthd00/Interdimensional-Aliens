e16a2d6d7d4abef845bb3f32df6dad6593cb5aac
Paarth <Paarthcode@gmail.com>
2021-06-25 22:42:41 -0700

    spinner

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Spinner from 'react-native-loading-spinner-overlay';
+
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
@@ -9,8 +12,19 @@ import Navigation from './navigation';
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const [spinner, setSpinner] = useState(false);
+  useEffect(() => {
+    setInterval(() => {
+      setSpinner(!spinner);
+    }, 3000);
+  });
 
   if (!isLoadingComplete) {
+    <Spinner
+    visible={spinner}
+    textContent={'Loading...'}
+    textStyle={styles.spinnerTextStyle}
+  />
     return null;
   } else {
     return (
@@ -21,3 +35,8 @@ export default function App() {
     );
   }
 }
+const styles = StyleSheet.create({
+  spinnerTextStyle: {
+    color: '#FFF'
+  }
+});
